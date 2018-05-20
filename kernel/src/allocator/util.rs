77f5ad0b586c87ef590a2563dd02108269096369
Hung-I Wang <i@hung-i.wang>
2018-05-20 15:22:30 +0800

    implement kernel::allocator::util (align_up, align_down)

--- kernel/src/allocator/util.rs
+++ kernel/src/allocator/util.rs
@@ -6,7 +6,8 @@
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_down(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    panic_if_not_power_of_two(align);
+    (addr / align) * align
 }
 
 /// Align `addr` upwards to the nearest multiple of `align`.
@@ -17,5 +18,18 @@ pub fn align_down(addr: usize, align: usize) -> usize {
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_up(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    match align_down(addr, align) {
+        aligned if aligned == addr => aligned,
+        aligned => aligned + align
+    }
+}
+
+fn is_power_of_two(number: usize) -> bool {
+    number & (number - 1) == 0
+}
+
+fn panic_if_not_power_of_two(number: usize) {
+    if !is_power_of_two(number) {
+        panic!("Wrong alignment size: {}! It can only be a power of two.", number);
+    }
 }
